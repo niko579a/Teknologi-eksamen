f2015b7e96f4036bcd007797b154574062789a05
niko579a <nikoline.stenfeldt@gmail.com>
2021-05-03 22:35:26 +0200

    Tilføjet selve beregningerne

--- js/calFood.js
+++ js/calFood.js
@@ -2,13 +2,14 @@ let amountOfPeople = 0;
 
 let allPersoninputs = [];
 
-function getChoosenFood() {
+function getchoosenRecipe() {
     let URL = window.location.href;
     let  slashRemove = URL.split('/');
-    return slashRemove[slashRemove.length -1].split('?')[1];
+     let recipe = slashRemove[slashRemove.length -1].split('?')[1];
+     return allRecipies[recipe];
 }
 
-const choosenFood = getChoosenFood();
+const choosenRecipe = getchoosenRecipe();
 
 const peopleAmountSelect = document.getElementById('peopleAmount');
 
@@ -27,6 +28,12 @@ function peopleAmountSelected() {
     personDetailsDiv.innerHTML = '';
     createInputs();
     personDetailsDiv.hidden = false;
+}
+
+function handelRangeInput(range, index){
+    allPersoninputs[index][range.name] = (range.value/100);
+    let span = document.getElementById((index + 1) + '-foodPriorty-span');
+    span.innerHTML = allPersoninputs[index][range.name];
 }
 
 function createInputs(){
@@ -50,7 +57,36 @@ function handelInput(input, index) {
 }
 
 function calFood() {
-    console.log(allPersoninputs);
+    let totalMealPriority = 0;
+    let totalCaloriesPrGramPrIngredients = 0;
+    let CaloriesPrGramPrIngredient = 0;
+    let caloriesPrPart = 0;
+
+    allPersoninputs.forEach(person => {
+        //En persons daglige kalorieindtag
+        if (person.gender == 'male') {
+            person.calorieAmount = 9.99 * person.weight + 6.25 * person.height - 4.92 * person.age + 5;
+        } else {
+            person.calorieAmount = 9.99 * person.weight + 6.25 * person.height - 4.92 * person.age - 161;
+        }
+        //Hvor mange kalorier personen skal spise dette måltid
+        person.mealPriority = person.calorieAmount * person.foodPriorty;
+    });
+
+    //Finder den totale mængde af hver person prioritet for et måltid
+    allPersoninputs.forEach(person => {
+        totalMealPriority += person.mealPriority;
+    });
+
+    //Vi lægger alle værdierne i arrayen sammen
+    choosenRecipe['caloriesPrGramPrIngredients'].forEach(value => {
+        totalCaloriesPrGramPrIngredients += value; 
+    });
+
+    //dividerer antallet af ingredienser kalorier med den samlet værdi af gram pr kalorie
+    CaloriesPrGramPrIngredient = totalCaloriesPrGramPrIngredients / choosenRecipe.amountOfIngredients.length;
+    
+    //Dividerer antal af folks prioterede mad med forrige resultat
+    caloriesPrPart = totalMealPriority / CaloriesPrGramPrIngredient;
+
 }
-
-
--- js/person.js
+++ js/person.js
@@ -4,12 +4,14 @@ class Person{
         this.ageInput= `Alder:<br><input onInput="handelInput(this, ${id - 1})" name="age" id="${id}-age" type="number">`,
         this.weightInput= `Vægt:<br><input onInput="handelInput(this, ${id - 1})" name="weight" id="${id}-weight" type="number">`,
         this.heightInput= `Højde:<br><input onInput="handelInput(this, ${id - 1})" name="height" id="${id}-height" type="number">`,
-        this.caloriesInput= `Antal kalorier på en dag:<br><input onInput="handelInput(this, ${id - 1})" name="calories" id="${id}-calories" type="number">`,
+       this.foodPriortyInput=`Hvordan priorter dette måltid ift. de to andre:<br><input onInput="handelRangeInput(this, ${id - 1})" name="foodPriorty" id="${id}-foodPriorty" type="range" min="0" maks="1" value="0"> <span id="${id}-foodPriorty-span"></span>`, 
+        this.foodPriorty= 0,
         this.gender = '',
         this.age = 0,
         this.weight = 0,
         this.height = 0,
-        this.calories = 0
+        this.calorieAmount  = 0,    
+        this.mealPriority = 0
     }
 }
 
